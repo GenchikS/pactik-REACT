df8abc39eec856079fa6da4c22339399d70acad4
Gennadii_S <106490249+GenchikS@users.noreply.github.com>
2024-08-14 00:45:47 +0300

    corr

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,10 +1,33 @@
 // import { useState } from 'react'
 import './App.css'
+import Product from './product/Product';
+import BookList from "./product/Product"
+import favouriteBooks from "./product/favouriteBooks.json"
 
 export default function App() {
-   return (
-    console.log("Hello")
-  )
+  return (
+    <div>
+      {/* <h1>Best selling</h1>
+      <Product
+        name="Tacos With Lime"
+        imgUrl="https://images.pexels.com/photos/461198/pexels-photo-461198.jpeg?dpr=2&h=480&w=640"
+        price={10.99}
+      />
+      <Product
+        name="Fries and Burger"
+        imgUrl="https://images.pexels.com/photos/70497/pexels-photo-70497.jpeg?dpr=2&h=480&w=640"
+        price={14.29}
+      /> */}
+      {/* приклад ренденгу масиву .map */}
+      {/* {[1, 2, 3].map((item) => {
+        return <p>{item}</p>;
+      })} */}
+      <>
+        <h1>Books of the week</h1>
+        <BookList books={favouriteBooks} />
+      </>
+    </div>
+  );
 }
 
 
---
+++ vite-project/src/product/Product.jsx
@@ -0,0 +1,111 @@
+//  приклад 1
+// export default function Product({ name, imgUrl, price }) {
+//      return (
+//     <div>
+//           <h2>{name}</h2>
+//           <img src={imgUrl} alt={name} width="320" />
+//         <p>Price: {price} credits</p>
+//     </div>
+//   );
+// }
+
+//  приклад 1  імпортування ф-ції Product та передача ключів
+// import Product from "./product/Product";
+// export default function App() {
+//   return (
+//     <div>
+//       <h1>Products</h1>
+//       <Product name="Tacos With Lime"
+//       imgUrl = "https://images.pexels.com/photos/461198/pexels-photo-461198.jpeg?dpr=2&h=480&w=640"
+//         price={10.99}
+//       />
+//       <Product name="Fries and Burger"
+//       imgUrl = "https://images.pexels.com/photos/70497/pexels-photo-70497.jpeg?dpr=2&h=480&w=640"
+//         price={14.99}
+//       />
+//     </div>
+//   );
+// }
+
+//  приклад 2
+// const username = "Nick"
+// // const messages = "Hello"
+// const messages = 0
+
+// const MailBox = () => {
+//   return (
+//     <div>
+//       <p>Hello {username}</p>
+//       {messages.length > 0 ? `You have ${messages.length} unread messages` : `No unread messages`}
+//       </div>)
+// }
+// export default MailBox
+
+
+//  приклад методу map
+// export default function MailBox(){
+//   return (
+//     <div>
+//       {[1, 2, 3].map(item => {
+//         return <div>{item}</div>
+//       })}
+//       </div>
+// )}
+
+
+//  приклад позбирання масиву об'єктів
+// const favouriteBooks = [
+//   { id: "id-1", name: "JS for beginners" },
+//   { id: "id-2", name: "React basics" },
+//   { id: "id-3", name: "React Router overview" }
+// ];
+
+// const BookList = ({ books }) => {
+//   return (
+//     <ul>
+//       {books.map((book) => {
+//         return (
+//             <li key={book.id}>{book.name}</li>
+//         )
+//         })}
+//     </ul>
+//   )
+// }
+
+// export default function App(){
+//   return (
+//     <div>
+//       <p>Books of the week</p>
+//       <BookList books = {favouriteBooks}/>
+//     </div>
+//   )
+// }
+
+
+//  приклад використання індекса масива, використовується крайні рідко
+// const favouriteBooks = [
+//   { name: "JS for beginners" },
+//   { name: "React basics" },
+//   { name: "React Router overview" }
+// ];
+
+// const BookList = ({ books }) => (
+//   <ul>
+// 	{books.map((book, index) => {
+// 	  return <li key={index}>{book.name}</li>;
+// 	})}
+//   </ul>
+// );
+
+
+export default function BookList({ books }){
+  return (
+    <ul>
+      {books.map((book) => {
+        return <li key={book.id}>{book.name}</li>;
+      })}
+    </ul>
+  );
+};
+
+
